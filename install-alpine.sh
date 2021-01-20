ff67b21983a295a5a64e4066cfc499f95eca66fa
Jason Dogariu <j.dogariu@kunzleigh.com>
2021-01-20 16:26:28 -0500

    Fix alpine dependencies

--- install-alpine.sh
+++ install-alpine.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 apk add php composer yarn
-apk add php-ctype php-sqlite3 php-fpm php-pdo_sqlite php-gd php-fileinfo php-tokenizer
+apk add php-ctype php-sqlite3 php-fpm php-pdo_sqlite php-gd php-fileinfo php-tokenizer php-simplexml
 
+compoer update
 composer install
 yarn
